d8800d1176385740c7a634829ed9f999fe05f161
edwin rivera <waehake@gmail.com>
2018-01-18 14:36:54 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,4 +1,10 @@
 // Add your doToElementsInArray() function here:
-
+function doToElementsInArray(items, callback) {
+  items.forEach(callback);
+  return items;
+};
 
 // Add your changeCompletely() function here:
+function changeCompletely() {
+  
+};
